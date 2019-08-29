0bc8aabb21d7f55d0e31b11b6dcd34db265cdcad
hani-mansour <hani.mansour.1990@gmail.com>
2019-08-29 16:09:53 +0200

    old TODO removed

--- weekday.php
+++ weekday.php
@@ -1,41 +1,8 @@
 <?php
+
 /**
  * Wochentagsberechnung nach https://de.wikipedia.org/wiki/Wochentagsberechnung
  */
-
-function main($argv,$argc): void
-{
-    setlocale(LC_TIME, 'de_AT.utf-8');
-
-    $day = $argv[1];
-    $month = $argv[2];
-    $year = $argv[3]; /* muss vierstellig sein */
-
-    if ($argc < 4 || $argc > 5) {
-        echo "Wrong number of arguments.";
-        exit(1);
-    }
-
-    $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
-    $c = substr($year, 0, 2);
-    if ($m >= 11) {
-        $c = substr($year - 1, 0, 2);
-    }
-    $y = substr($year, 2, 2);
-    if ($m >= 11) {
-        $y = substr($year - 1, 2, 2);
-    }
-
-    $weekdayNumber = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
-    $weekday = getWeekdayName($weekdayNumber);
-    printEingabe($day, $month, $year);
-
-    printAusgabe($weekday);
-
-    printDebugOutput($m, $y, $c);
-}
-
-main($argv,$argc);
 
 /**
  * @param int $w
@@ -68,6 +35,49 @@ function printDebugOutput(int $m, $y, $c): void
     }
 }
 
+function main(int $argc, array $argv): void
+{
+    setlocale(LC_TIME, 'de_AT.utf-8');
 
+    list($day, $month, $year) = handleCommandLine($argc, $argv);
 
+    $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
+    //TODO: repair the double if ($m checked twice)
+    if ($m >= 11) {
+        $c = substr($year - 1, 0, 2);
+        $y = substr($year - 1, 2, 2);
+    } else {
+        $c = substr($year, 0, 2);
+        $y = substr($year, 2, 2);
+    }
 
+    $weekdayNumber = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+
+    $weekday = getWeekdayName($weekdayNumber);
+
+    printEingabe($day, $month, $year);
+
+    printAusgabe($weekday);
+
+    printDebugOutput($m, $y, $c);
+}
+
+/**
+ * @param int $argc
+ * @param array $argv
+ * @return array
+ */
+function handleCommandLine(int $argc, array $argv): array
+{
+    $day = $argv[1];
+    $month = $argv[2];
+    $year = $argv[3]; /* muss vierstellig sein */
+
+    if ($argc < 4 || $argc > 5) {
+        echo "Wrong number of arguments.";
+        exit(1);
+    }
+    return [$day, $month, $year];
+}
+
+main($argc,$argv);
